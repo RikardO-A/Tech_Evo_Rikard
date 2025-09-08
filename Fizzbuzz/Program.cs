7936e8bde30dbb7189a06f06ff676a4110630c2e
Rikard O.A <rikard.okello-akanga@sveaskog.se>
2025-09-08 14:42:33 +0200

    gitignore

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿//Fizzbuzz
+// Delbart med 3. print Fizz
+//Delbart med 5, print Buzz
+//Om båda, fizzbuzz
+//1-100
+// OM x/3 = heltal, print Fizz
+// OM x/5 = heltall, print Buzz
+// OM x/3 && x/5 = heltal, print FizzBuzz
+//For i - X
+
+
+for (int x = 1; x <= 100; x++)
+{
+    int modulo3 = x % 3;
+    int modulo5 = x % 5;
+
+    if (modulo3 == 0 && modulo5 == 0)
+        Console.WriteLine("FizzBuzz");
+    else if (modulo3 == 0)
+        Console.WriteLine("Fizz");
+    else if (modulo5 == 0)
+        Console.WriteLine("Buzz");
+    else
+        Console.WriteLine(x);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Tech_Evo_Rikard
+
+uppgift 0 test
